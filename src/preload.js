700c2219ff0f377a8c4bb7956826371832acc7c7
waseem abbas <waseemofficee@gamil.com>
2026-04-06 19:33:21 +0500

    refactor: remove unused note-related IPC methods from preload script

--- src/preload.js
+++ src/preload.js
@@ -5,11 +5,7 @@ contextBridge.exposeInMainWorld('electron', {
   close: () => ipcRenderer.send('window-close'),
   setWindowHeight: (height) => ipcRenderer.send('window-set-height', height),
   setWindowSize: (width, height) => ipcRenderer.send('window-set-size', { width, height }),
-  detachNote: (note) => ipcRenderer.send('note-detach', note),
-  setPinnedNote: (noteId, pinned) => ipcRenderer.send('note-set-pinned', { noteId, pinned }),
-  toggleStandaloneAlwaysOnTop: (noteId, isAlwaysOnTop) => ipcRenderer.send('note-toggle-always-on-top', { noteId, isAlwaysOnTop }),
   getAppSettings: () => ipcRenderer.invoke('get-app-settings'),
   updateAppSettings: (settings) => ipcRenderer.invoke('update-app-settings', settings),
   getAppVersion: () => ipcRenderer.invoke('get-app-version'),
-  deleteNote: (noteId) => ipcRenderer.send('note-delete', noteId),
 });
